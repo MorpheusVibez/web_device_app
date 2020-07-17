bd51f7e613704138c0d6f3719e8a4481057e869b
MorpheusVibez <halodre24@gmail.com>
2020-07-16 22:00:25 -0400

    Created Device model | Create Seeds

---
+++ db/migrate/20200717014242_create_devices.rb
@@ -0,0 +1,12 @@
+class CreateDevices < ActiveRecord::Migration[6.0]
+  def change
+    create_table :devices do |t|
+      t.string :name
+      t.string :storage_size
+      t.string :color
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user1 = User.create(username: "User1", password_digest: "1234", name: "First User", location: "Springfield, MA")
+user2 = User.create(username: "User2", password_digest: "abcd", name: "Second User", location: "Atlanta, GA")
+user3 = User.create(username: "User3", password_digest: "wxyz", name: "Third User", location: "Los Angeles, CA")
+
+device1 = Device.create(name: "Macbook Pro", storage_size: "500GB", color: "Space Grey")
+device2 = Device.create(name: "iPhone X", storage_size: "64GB", color: "Space Grey")
+device3 = Device.create(name: "iPad Pro", storage_size: "64GB", color: "Silver")
