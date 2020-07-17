cf73911ed8c352cd7cb6cee8142cbd3485e47960
MorpheusVibez <halodre24@gmail.com>
2020-07-17 11:26:21 -0400

    Set up login and homepage route

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  post '/logout' => 'sessions#destroy'
+
+  root 'homepage#home'
+
   resources :users
+
 
 
   resources :devices
 
-  
+
   resources :apps
 
 end
