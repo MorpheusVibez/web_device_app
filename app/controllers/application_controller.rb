471fd775118b1f26002f98b548df1a6c9b53b488
MorpheusVibez <halodre24@gmail.com>
2020-07-17 11:25:49 -0400

    Set up sessions for user login | Set up methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+    def current_user 
+        @user = (User.find_by(id: session[:user_id]) || User.new)
+      end 
+      
+      def logged_in?
+        current_user.id != nil 
+        #return true if the current user's id from the db is not nil 
+      end 
+      
+      def require_logged_in 
+        return redirect_to(controller: 'sessions', action: 'new') unless logged_in?
+      end 
+      #show them the new page unless they're logged in 
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,26 @@
 class SessionsController < ApplicationController
+    def new
+
+    end
+
+    def create
+        user = User.find_by(username: params[:user][:username])
+          
+        user = user.try(:authenticate, params[:user][:password])
+    
+        return redirect_to(controller: 'sessions', action: 'new') unless user
+        
+        session[:user_id] = user.id
+    
+        @user = user
+    
+        redirect_to controller: 'homepage', action: 'home'
+        #take them to their home page 
+      end
+    
+      def destroy
+        session.delete :user_id
+        
+        redirect_to '/'
+      end
 end
