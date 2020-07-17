aecdf62278d534734def5fcb8b12cd0998b5461b
MorpheusVibez <halodre24@gmail.com>
2020-07-17 10:22:25 -0400

    Added validations

--- app/models/app.rb
+++ app/models/app.rb
@@ -1,4 +1,9 @@
 class App < ApplicationRecord
     belongs_to :devices, optional: :true
-    belongs_to :users
+    belongs_to :users, optional: :true
+
+    validates :name, presence: :true
+    validates :description, presence: :true
+    validates :category, presence: :true
+    validates :storage_size, presence: :true
 end
