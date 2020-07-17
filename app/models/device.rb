3eb924a6cdc6c61d371d3ad20c995e4022c8b9c8
MorpheusVibez <halodre24@gmail.com>
2020-07-17 10:23:08 -0400

    Added validations

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,4 +1,9 @@
 class Device < ApplicationRecord
     belongs_to :users, optional: :true
     has_many :apps, through: :users
+
+    validates :name, presence :true
+    validates :storage_size, presence :true
+    validates :color, presence :true
+    
 end
