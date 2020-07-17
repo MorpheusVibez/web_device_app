78152ea24cffdbb24b91188f068cf5efbdd860d9
MorpheusVibez <halodre24@gmail.com>
2020-07-17 09:41:17 -0400

    Building users/new route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,18 @@
 class UsersController < ApplicationController
 
     def new
-
+        @user = User.new
     end
 
     def create
+        @user = User.create(user_params)
+        session[:user_id] = @user.id
 
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :password, :password_confirmation :name, :location)
     end
 end
