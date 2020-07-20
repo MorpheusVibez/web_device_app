3041d85611ca112f8ec78adb256dc95169a1d09f
MorpheusVibez <halodre24@gmail.com>
2020-07-20 10:58:02 -0400

    Setting up OmniAuth Github login

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   post '/logout' => 'sessions#destroy'
+  get 'auth/github/callback', to: 'sessions#create'
 
   root 'homepage#home'
 
